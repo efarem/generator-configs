af430f7346d72c425e49db9cc683f8ee15a098ed
Frank Martin <frank.martin@digital-results.com>
2016-06-07 20:22:04 +0100

    Choose what you want to include

--- app/index.js
+++ app/index.js
@@ -1,19 +1,42 @@
 var yo = require('yeoman-generator');
 var yosay = require('yosay');
+var pkg = require('../package.json');
 
 module.exports = yo.Base.extend({
   constructor: function construct() {
     yo.Base.apply(this, arguments);
   },
-  initializing: function () {
-    this.pkg = require('../package.json');
+  initializing: function initializing() {
+    this.pkg = pkg;
   },
-  prompting: function () {
-    this.log(yosay('\'Allo \'allo! Config files coming right up...'));
-
+  prompting: function prompting() {
     var done = this.async();
-
     var prompts = [{
+      type: 'checkbox',
+      name: 'features',
+      message: 'What shall I include?',
+      choices: [{
+        name: 'ESLint',
+        value: 'includeEslint',
+        checked: true
+      }, {
+        name: 'Editor Config',
+        value: 'includeEditorconfig',
+        checked: true
+      }, {
+        name: 'SCSS Lint',
+        value: 'includeScss',
+        checked: true
+      }, {
+        name: 'Gulp',
+        value: 'includeGulp',
+        checked: true
+      }, {
+        name: 'Git Ingore',
+        value: 'includeGit',
+        checked: true
+      }]
+    }, {
       type: 'list',
       name: 'ecmaversion',
       message: 'Which version of ECMAScript?',
@@ -23,53 +46,87 @@ module.exports = yo.Base.extend({
       }, {
         name: 'ES5',
         value: 5
-      }]
+      }],
+      when: function when(answers) {
+        return answers.features.indexOf('includeEslint') !== -1;
+      }
     }];
 
-    this.prompt(prompts, function (answers) {
+    this.log(yosay('\'Allo \'allo! Config files coming right up...'));
+
+    this.prompt(prompts, function handleAnswers(answers) {
+      var features = answers.features;
+
       this.ecmaversion = answers.ecmaversion;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.includeEslint = hasFeature('includeEslint');
+      this.includeEditorconfig = hasFeature('includeEditorconfig');
+      this.includeScss = hasFeature('includeScss');
+      this.includeGulp = hasFeature('includeGulp');
+      this.includeGit = hasFeature('includeGit');
+
       done();
     }.bind(this));
-
   },
   writing: {
-    global: function () {
-      this.fs.copyTpl(
-        this.templatePath('.editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copyTpl(
-        this.templatePath('.gitignore'),
-        this.destinationPath('.gitignore')
-      );
-      this.fs.copyTpl(
-        this.templatePath('.scss-lint.yml'),
-        this.destinationPath('.scss-lint.yml')
-      );
-      this.fs.copyTpl(
-        this.templatePath('package.json'),
-        this.destinationPath('package.json'),
-        { ecmaversion: this.ecmaversion }
-      );
-      this.fs.copyTpl(
-        this.templatePath('gulpfile.babel.js'),
-        this.destinationPath('gulpfile.babel.js')
-      );
+    global: function global() {
+      if (this.includeEditorconfig) {
+        this.fs.copyTpl(
+          this.templatePath('.editorconfig'),
+          this.destinationPath('.editorconfig')
+        );
+      }
+
+      if (this.includeGit) {
+        this.fs.copyTpl(
+          this.templatePath('.gitignore'),
+          this.destinationPath('.gitignore')
+        );
+      }
+
+      if (this.includeScss) {
+        this.fs.copyTpl(
+          this.templatePath('.scss-lint.yml'),
+          this.destinationPath('.scss-lint.yml')
+        );
+      }
+
+      if (this.includeGulp) {
+        this.fs.copyTpl(
+          this.templatePath('gulpfile.babel.js'),
+          this.destinationPath('gulpfile.babel.js')
+        );
+      }
     },
-    versionDependent: function () {
-      switch (this.ecmaversion) {
-        case 6:
-          this.fs.copyTpl(this.templatePath('es6/.eslintrc.json'), this.destinationPath('.eslintrc.json'));
-          this.fs.copyTpl(this.templatePath('es6/.babelrc'), this.destinationPath('.babelrc'));
-        break;
-        default:
-          this.fs.copyTpl(this.templatePath('es5/.eslintrc.json'), this.destinationPath('.eslintrc.json'));
-          this.fs.copyTpl(this.templatePath('es5/.babelrc'), this.destinationPath('.babelrc'));
-        break;
+    versionDependent: function versionDependent() {
+      if (this.includeEslint) {
+        switch (this.ecmaversion) {
+          case 6:
+            this.fs.copyTpl(
+              this.templatePath('es6/.eslintrc.json'),
+              this.destinationPath('.eslintrc.json')
+            );
+            this.fs.copyTpl(
+              this.templatePath('es6/.babelrc'),
+              this.destinationPath('.babelrc')
+            );
+            break;
+          default:
+            this.fs.copyTpl(
+              this.templatePath('es5/.eslintrc.json'),
+              this.destinationPath('.eslintrc.json')
+            );
+            this.fs.copyTpl(
+              this.templatePath('es5/.babelrc'),
+              this.destinationPath('.babelrc')
+            );
+            break;
+        }
       }
     }
-  },
-  end: function () {
-
   }
 });
